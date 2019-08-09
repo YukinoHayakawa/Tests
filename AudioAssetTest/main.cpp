d45f40a8e8accc37e3c542f3751054bcb2db44b4
Yukino Hayakawa <tennencoll@gmail.com>
2019-08-09 19:11:09 +0800

    add audio asset test

---
+++ AudioAssetTest/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <fstream>
+#include <filesystem>
+
+#include <Usagi/Utility/Utf8Main.hpp>
+#include <Usagi/Extensions/AssetAudioFormats/LibNyquistAudioAssetDecoder.hpp>
+#include <Usagi/Core/Logging.hpp>
+
+using namespace usagi;
+
+int usagi_main(const std::vector<std::string> &args)
+{
+    LibNyquistAudioAssetDecoder decoder;
+    auto path = std::filesystem::u8path(args[0]);
+    std::ifstream in(path, std::ios::binary);
+    auto audio = decoder(in);
+
+    LOG(info, "File:        {}", path);
+    LOG(info, "Channels:    {}", audio.format.num_channels);
+    LOG(info, "Sample Rate: {}", audio.format.sample_rate);
+    LOG(info, "Interleaved: {}", audio.format.interleaved);
+    LOG(info, "Format:      {}", to_string(audio.format.format));
+    LOG(info, "Frame Count: {}", audio.num_frames);
+
+    return 0;
+}
